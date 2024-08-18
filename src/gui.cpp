cd42253ddeec81498b7f375b807fbaa1dd74f509
Gorka Liul <gorkaliul@gmail.com>
2024-08-18 21:40:15 +0200

    more 3d!

--- src/gui.cpp
+++ src/gui.cpp
@@ -189,5 +189,3 @@ bool ui::editor(){
 
 }
 
-
-
--- src/main.cpp
+++ src/main.cpp
@@ -63,13 +63,27 @@ float vertices[] = {
     -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 };
 
-bool wireframeEnable = false;
+glm::vec3 cubePositons[] = {
+  glm::vec3( 0.0f,  0.0f,  0.0f), 
+    glm::vec3( 2.0f,  5.0f, -15.0f), 
+    glm::vec3(-1.5f, -2.2f, -2.5f),  
+    glm::vec3(-3.8f, -2.0f, -12.3f),  
+    glm::vec3( 2.4f, -0.4f, -3.5f),  
+    glm::vec3(-1.7f,  3.0f, -7.5f),  
+    glm::vec3( 1.3f, -2.0f, -2.5f),  
+    glm::vec3( 1.5f,  2.0f, -2.5f), 
+    glm::vec3( 1.5f,  0.2f, -1.5f), 
+    glm::vec3(-1.3f,  1.0f, -1.5f)
+};
+
+
 int main(){
 
   Window Win_class(1500, 700, "arcane Engine");
   Shader shaderProgram("../shaders/vertexShader.txt", "../shaders/fragmentShader.txt");
   ui myGui(Win_class.window);
 
+  bool wireframeEnable = false;
   glfwSwapInterval(0); //disables vsync
   // creating the vertex buffer object (loads memory cpu -> GPU)
   unsigned int VBO, VAO, EBO;
@@ -167,18 +181,10 @@ int main(){
 shaderProgram.use();
       
         // create transformations
-        glm::mat4 model         = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
-        glm::mat4 view          = glm::mat4(1.0f);
         glm::mat4 projection    = glm::mat4(1.0f);
-        model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-        view  = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-        projection = glm::perspective(glm::radians(45.0f), (float)width / (float)height, 0.1f, 100.0f);
+        projection = glm::perspective(glm::radians(65.0f), (float)width / (float)height, 0.1f, 100.0f);
         // retrieve the matrix uniform locations
-        unsigned int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
-        unsigned int viewLoc  = glGetUniformLocation(shaderProgram.ID, "view");
         // pass them to the shaders (3 different ways)
-        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
         // note: currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
         shaderProgram.setMat4("projection", projection);
 
@@ -226,7 +232,17 @@ shaderProgram.use();
   //transformations & perspective
 
     glBindVertexArray(VAO);
+    for(int i = 0; i<sizeof(cubePositons)/(3*sizeof(float)) ; i++){
+      glm::mat4 model = glm::mat4(1.0f);
+      model = glm::translate(model, cubePositons[i]);
+      float angle = 20.0f*i;
+      if(i%3){
+        angle = (float)glfwGetTime()*50;
+      }
+      model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+      shaderProgram.setMat4("model", model);
       glDrawArrays(GL_TRIANGLES,0, 36);
+    }
 
     myGui.mainGui();
       glfwSwapBuffers(Win_class.window);
